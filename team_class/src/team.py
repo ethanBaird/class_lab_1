9d44d3dbf141a4ee7110988a5d297e50f24d5441
Ethan <ethanbaird94@gmail.com>
2022-06-27 13:28:23 +0100

    passall team tests

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -3,5 +3,20 @@ class Team():
         self.name = name
         self.players = players
         self.coach = coach 
+        self.points = 0
 
-    
+
+    def add_player(self, new_player):
+        self.players.append(new_player)
+
+    def has_player(self, player):
+        for each_player in self.players:
+            if player == each_player:
+                return True
+        return False
+
+    def play_game(self, win):
+        if win: self.points += 3 
+        else: pass
+
+        
